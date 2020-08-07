45f5b4825dc2f14ddc2cb6be88a8b99d6d783163
Alex Pop <a.pop@zengenti.com>
2020-08-07 15:02:20 +0100

    updated examples for roles

--- express-test-app/examples/roles.js
+++ express-test-app/examples/roles.js
@@ -1,17 +1,133 @@
- // client.roles.list({ pageIndex: 1, pageSize: 2 })
+exports.roles_get = (client, res) => {
+    return client.roles.get('c31111e7-76f7-46dd-93fb-cbf81a853a37')
+        .then(result => {
+            console.log('API call result: ', result);
+            if (res) {
+                res.render('index', { title: 'Success' });
+            }
+            return result;
+        })
+        .catch(error => {
+            console.log('API call error: ', error);
+            if (res) {
+                res.render('index', { title: 'Error' });
+            }
+        });
+}
 
-  // client.roles.get('da1c235d-af39-497e-8d0d-b5a5e4f36ca1')
+exports.roles_create = (client, res) => {
+    let role = {
+        "name": {
+            "en-GB": "Movie Editors"
+        },
+        "description": {
+            "en-GB": "Movie editors can edit movies, but not submit or approve them"
+        },
+        "enabled": true,
+        "permissions": {
+            "entries": [
+                {
+                    "id": "movie",
+                    "languages": ["en-GB"],
+                    "actions": ["sys.update", "awaitingApproval.revoke"]
+                }
+            ],
+            "contentTypes": []
+        },
+        "assignments": {
+            "users": ["a.user"],
+            "groups": ["Movie Editors"],
+            "apiKeys": ["Movie Import"]
+        }
+    };
+    return client.roles.create(role)
+        .then(result => {
+            console.log('API call result: ', result);
+            if (res) {
+                res.render('index', { title: 'Success' });
+            }
+            return result;
+        })
+        .catch(error => {
+            console.log('API call error: ', error);
+            if (res) {
+                res.render('index', { title: 'Error' });
+            }
+        });
+}
 
-  // client.roles.get('da1c235d-af39-497e-8d0d-b5a5e4f36ca1')
-  // .then(result => {
-  //   if (result) {
-  //     delete result.id;
-  //     result.name['en-GB'] = result.name['en-GB'] + ' Copy';
-  //     client.roles.create(result)
+exports.roles_update = (client, res) => {
+    let role = {
+        "id": "71b48d24-7f65-457d-bd51-cba977b74b74",
+        "name": {
+            "en-GB": "Movie Editors"
+        },
+        "description": {
+            "en-GB": "Movie editors can edit movies, but not submit or approve them"
+        },
+        "enabled": true,
+        "permissions": {
+            "entries": [
+                {
+                    "id": "movie",
+                    "languages": ["*"],
+                    "actions": ["draft.*", "awaitingApproval.revoke"]
+                }
+            ],
+            "contentTypes": []
+        },
+        "assignments": {
+            "users": ["a.user"],
+            "groups": ["Movie Editors"],
+            "apiKeys": ["Movie Import"]
+        }
+    };
+    return client.roles.update(role)
+        .then(result => {
+            console.log('API call result: ', result);
+            if (res) {
+                res.render('index', { title: 'Success' });
+            }
+            return result;
+        })
+        .catch(error => {
+            console.log('API call error: ', error);
+            if (res) {
+                res.render('index', { title: 'Error' });
+            }
+        });
+}
 
+exports.roles_delete = (client, res) => {
+    return client.roles.delete('c31111e7-76f7-46dd-93fb-cbf81a853a37')
+        .then(result => {
+            console.log('API call was successful');
+            if (res) {
+                res.render('index', { title: 'Success' });
+            }
+            return result;
+        })
+        .catch(error => {
+            console.log('API call error: ', error);
+            if (res) {
+                res.render('index', { title: 'Error' });
+            }
+        });
+}
 
-  // client.roles.get('8c5d47b0-0700-40a4-9dbd-a3d21b45bf14')
-  //   .then(result => {
-  //     if (result) {        
-  //       result.name['en-GB'] = result.name['en-GB'] + ' v1';
-  //       client.roles.update(result)
+exports.roles_list = (client, res) => {
+    return client.roles.list({ pageIndex: 1, pageSize: 5 })
+        .then(result => {
+            console.log('API call result: ', result);
+            if (res) {
+                res.render('index', { title: 'Success' });
+            }
+            return result;
+        })
+        .catch(error => {
+            console.log('API call error: ', error);
+            if (res) {
+                res.render('index', { title: 'Error' });
+            }
+        });
+}
